29fa5cf3557d67f09aaf8d847bcbd57a5112fab1
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-03-21 13:26:12 +0530

    LinkedList

---
+++ Lec19/FindKth.java
@@ -0,0 +1,57 @@
+package Lec19;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
+
+public class FindKth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Node head = null;
+        Node tail = null;
+
+        int data = sc.nextInt();
+        while (data != -1) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            data = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+        int result = findKthFromLast(head, k);
+        System.out.println(result);
+    }
+
+    public static int findKthFromLast(Node head, int k) {
+        Node slow = head;
+        Node fast = head;
+
+        for (int i = 0; i < k; i++) {
+            if (fast == null) {
+                return -1; // k is greater than the length of the linked list
+            }
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow.data;
+    }
+}
