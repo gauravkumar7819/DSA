997f8d9f87e7b5060d8cd8e0e55abfc58617e33a
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-03-24 21:06:28 +0530

    recursion

---
+++ Revision/Oddeven.java
@@ -0,0 +1,27 @@
+package Revision;
+
+public class Oddeven {
+public static void main(String[] args) {
+    int n=5;
+    print(n);}
+    
+
+
+
+
+private static void print(int n) {
+if(n<=0){
+    return;
+}
+if (n%2==1) {
+    
+
+System.out.println( n);}
+print(n-1);
+if(n%2==0){
+System.out.println(n);
+
+}
+
+}
+}
--- Revision/Permutation.java
+++ Revision/Permutation.java
@@ -1,23 +1,26 @@
 package Revision;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutation {
 	public static void main(String[] args) {
 		String str="123";
+		List<String> list=new ArrayList<>();
 		
-		fn(str,"");
+		fn(str,"",list);
+		System.out.println(list);
 	}
 
-	private static void fn(String s, String ans) {
+	private static void fn(String s, String ans, List<String> list) {
 		if(s.length()==0) {
-			
+			list.add(ans);
 			System.out.println(ans);
 		
 		
 		
 		}
 		for (int j = 0; j < s.length(); j++) {
-			
-		
-		fn(s.substring(0, j)+s.substring(1+j),ans+s.charAt(j));
+		fn(s.substring(0, j)+s.substring(1+j),ans+s.charAt(j),list);
 	}}
 }
