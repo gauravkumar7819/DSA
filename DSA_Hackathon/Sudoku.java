bc6c76054d32d1c477296b3fd954c3b75dbcde6c
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-04-02 20:56:27 +0530

    revision

--- DSA_Hackathon/Sudoku.java
+++ DSA_Hackathon/Sudoku.java
@@ -88,3 +88,50 @@ for (int i = 0; i < grid.length; i++) {
 
     }
 }
+package Lec9;
+
+public class WordSearch {
+    static boolean solve(char[][] board,int row, int col, int idx,String word){
+        if(idx==word.length()){ 
+            return true;
+        }
+        if(row < 0 || col < 0 || row == board.length || col == board[0].length || board[row][col]=='*' || board[row][col]!=word.charAt(idx)){
+            return false;
+        }        
+        boolean flag = false;
+        char ch = board[row][col];
+        board[row][col]='*';
+        int[] r ={0,1,0,-1};
+        int[] c ={1,0,-1,0};
+        for(int i = 0;i<4;i++){
+            flag=solve(board, row+r[i], col+c[i], idx+1, word);
+            if (flag) {
+                return flag;
+            }
+        }
+        board[row][col]=ch;
+        return flag;
+    }
+    public static void main(String[] args) {
+        char[][] board ={{'A','B','C','E'},
+                         {'S','F','C','S'},
+                         {'A','D','E','E'}};
+        String word = "AB";
+        boolean flag=false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if(board[i][j]==word.charAt(0)){
+                   flag= solve(board,i,j,0,word);
+                   if (flag) {
+                    System.out.println(true);
+                    return;
+                }
+                  
+                }
+            }
+        }
+       
+        System.out.println(false);
+    }
+    
+}
