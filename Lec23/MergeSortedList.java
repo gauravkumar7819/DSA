1570d396f37c9274480c74e079ff1821a6cc1ba9
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-04-05 21:22:48 +0530

    LinkedList

---
+++ Lec23/MergeSortedList.java
@@ -0,0 +1,23 @@
+package Lec23;
+
+import Lec14.LinkedList;
+
+public class MergeSortedList {
+public static void main(String[] args) {
+    LinkedList ll=new LinkedList();
+    ll.addLast(10);
+    ll.addLast(20);
+    ll.addLast(30);
+    ll.addLast(40);
+    LinkedList l2=new LinkedList();
+    l2.addLast(10);
+    l2.addLast(20);
+    l2.addLast(30);
+    l2.addLast(40);
+    ll.display();
+    l2.display();
+    
+}
+    
+
+}
