66aef56d51fe8e09da0c71427e233fb57ba78cdf
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-04-12 14:14:15 +0530

    Stack

--- Lec25/K_beauty.java
+++ Lec25/K_beauty.java
@@ -1,13 +1,25 @@
 package Lec25;
 
 public class K_beauty {
+    static int count=0;
 public static void main(String[] args) {
-    String str="123";
-    print(str,"",0);
+    String str="43000043";
+    int n=Integer.parseInt(str);
+    print(str,"",n);
+    System.out.println(count);
 }
 
-private static void print(String str, String ans, int j) {
-    if (ans.length()==2) {
+private static void print(String str, String ans, int n) {
+    if (ans.length()==3) {
+        int m=Integer.parseInt(ans);
+        // System.out.println(n%m);
+
+    //    if (m!=0) {
+    //     if (n%m==0) {
+    //         count=count+1;
+            
+    //     }
+    //    }
         System.out.println(ans);
         
     }
@@ -15,11 +27,12 @@ private static void print(String str, String ans, int j) {
         return;
         
     }
-    int i=j;
-    for (i= 0; i < str.length(); i++) {
+    // int i=j;
+    
+    for (int i= 1; i < str.length(); i++) {
         
     
-    print(str.substring(i+1), ans+str.charAt(i), i);
+    print(str.substring(i+1), ans+str.substring(i, i+1), n);
     }
 }
     
